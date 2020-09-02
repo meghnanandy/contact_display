bd1349f95b87b6697d34b0712ee76c0092b46db1
Meghna Nandy <62709400+meghnanandy@users.noreply.github.com>
2020-09-02 08:13:47 +0530

    contactDisplay_file_Upload

---
+++ App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+import Avatar from "./Avatar";
+
+function creatCard(contact) {
+    return <Card
+        key={contact.name}
+        name={contact.name}
+        img={contact.imgURL}
+        tel={contact.phone}
+        email={contact.email}
+    />
+}
+
+function App() {
+  return (
+    <div>
+          <h1 className="heading">My Contacts</h1>
+          <Avatar
+              img= "https://scontent.fccu4-1.fna.fbcdn.net/v/t1.0-9/54433166_331688350815237_3164743323457945600_n.jpg?_nc_cat=104&_nc_sid=174925&_nc_ohc=yI63MZUVTDsAX95f7vk&_nc_ht=scontent.fccu4-1.fna&oh=9704539b6c704f8e2e6dc0f37e0250f4&oe=5F74F5A9"
+          />
+          {contacts.map(creatCard)}
+          
+
+    </div>
+  );
+}
+
+export default App;
---
+++ contacts.js
@@ -0,0 +1,25 @@
+const contacts = [
+  {
+    name: "Jung Hoseok",
+    imgURL:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQVK-t2K8k-05AX3Yz6rpr3rPnRt98vSKACNw&usqp=CAU",
+    phone: "+123 456 789",
+    email: "hobi@bts.com"
+  },
+  {
+      name: "Jisshu Sengupta",
+    imgURL:
+      "https://in.bmscdn.com/iedb/artist/images/website/poster/large/jisshu-sengupta-3213-28-03-2018-05-00-15.jpg",
+    phone: "+987 654 321",
+    email: "jsengupta@nowhere.com"
+  },
+  {
+    name: "Simon Baker",
+    imgURL:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Simon_Baker_2013_4_%28cropped%29.jpg/220px-Simon_Baker_2013_4_%28cropped%29.jpg",
+    phone: "+918 372 574",
+    email: "gmail@simonbaker.com"
+  }
+];
+
+export default contacts;
